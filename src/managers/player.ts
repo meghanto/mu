73ac51359dbd444a96f3d3f7fc08aacd5527b65d
Meghanto Majumder <meghanto20@gmail.com>
2025-11-14 12:30:47 -0600

    fix: add missing import and restore Player manager from mu-dev

--- src/managers/player.ts
+++ src/managers/player.ts
@@ -2,26 +2,52 @@ import {inject, injectable} from 'inversify';
 import {TYPES} from '../types.js';
 import Player from '../services/player.js';
 import FileCacheProvider from '../services/file-cache.js';
+import Config from '../services/config.js';
 
 @injectable()
-export default class {
+export default class PlayerManager {
   private readonly guildPlayers: Map<string, Player>;
+  private readonly pendingPlayers: Map<string, Promise<Player>>;
   private readonly fileCache: FileCacheProvider;
+  private readonly config: Config;
 
-  constructor(@inject(TYPES.FileCache) fileCache: FileCacheProvider) {
+  constructor(
+    @inject(TYPES.FileCache) fileCache: FileCacheProvider,
+    @inject(TYPES.Config) config: Config
+  ) {
     this.guildPlayers = new Map();
+    this.pendingPlayers = new Map();
     this.fileCache = fileCache;
+    this.config = config;
   }
 
-  get(guildId: string): Player {
-    let player = this.guildPlayers.get(guildId);
-
-    if (!player) {
-      player = new Player(this.fileCache, guildId);
-
-      this.guildPlayers.set(guildId, player);
+  async get(guildId: string): Promise<Player> {
+    const existing = this.guildPlayers.get(guildId);
+    if (existing) {
+      return existing;
     }
 
-    return player;
+    let pending = this.pendingPlayers.get(guildId);
+
+    if (!pending) {
+      pending = Player.create(this.fileCache, guildId, this.config)
+        .then(player => {
+          this.guildPlayers.set(guildId, player);
+          this.pendingPlayers.delete(guildId);
+          return player;
+        })
+        .catch(error => {
+          this.pendingPlayers.delete(guildId);
+          throw error;
+        });
+
+      this.pendingPlayers.set(guildId, pending);
+    }
+
+    return pending;
+  }
+
+  getAll(): Player[] {
+    return Array.from(this.guildPlayers.values());
   }
 }
